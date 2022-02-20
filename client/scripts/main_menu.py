240d89a546cdc1ee0af62e390f26c58e36507f04
Tom Draper <drapt002@gmail.com>
2022-02-20 19:19:19 +0000

    Added how to play button functionallity

--- client/scripts/main_menu.py
+++ client/scripts/main_menu.py
@@ -2,11 +2,21 @@ from PyQt5 import QtWidgets, uic
 import sys
 from client import functions as f
 
+class HTP_Dialogue_Box(QtWidgets.QDialog):
+    """Display the how to play dialog box"""
+    def __init__(self):
+        super(HTP_Dialogue_Box, self).__init__()
+        uic.loadUi('gui_files/how_to_play.ui', self)
+        self.setWindowTitle('How to Play')  # set title
+        self.show()
+        self.ok_button = self.findChild(QtWidgets.QPushButton, 'ok_button')
+        self.ok_button.clicked.connect(self.close)  # close dialogue box when ok is clicked
+
 
 class Main_Menu_Ui(QtWidgets.QMainWindow):
     def __init__(self, temp_username, temp_password):
-        super(Main_Menu_Ui, self).__init__() # Call the inherited classes __init__ method
-        uic.loadUi('gui_files/main_menu.ui', self) # Load the .ui file
+        super(Main_Menu_Ui, self).__init__()  # Call the inherited classes __init__ method
+        uic.loadUi('gui_files/main_menu.ui', self)  # Load the .ui file
         self.show()  # Show the GUI
         self.username = temp_username
         self.password = temp_password
@@ -24,10 +34,13 @@ class Main_Menu_Ui(QtWidgets.QMainWindow):
         self.refresh_button.clicked.connect(self.refresh_team_data)
         self.exit_button.clicked.connect(self.sign_out)
         #self.edit_team_button.clicked.connect(self.edit_team)
-        #self.how_to_play_button.clicked.connect(self.how_to_play)
+        self.how_to_play_button.clicked.connect(self.how_to_play)
         #self.recent_driver_changes_button.clicked.connect(self.recent_driver_changes)
         #self.how_val_calc_button.clicked.connect(self.how_val_calc)
 
+    def how_to_play(self):
+        self.htp_dialogue_box = HTP_Dialogue_Box()
+        self.htp_dialogue_box.show()  # show dialogue box
 
     def refresh_team_data(self):
         s = f.connect_to_server()
